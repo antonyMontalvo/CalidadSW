ebb2bc686fd437490359f4b0379aff00eb0b2ab9
Christian <arturogarcia_96@hotmail.com>
2019-11-17 18:45:50 -0500

    EventManagement date validation

--- src/main/java/unmsm/edu/pe/calidadsw/controller/EventManagementServlet.java
+++ src/main/java/unmsm/edu/pe/calidadsw/controller/EventManagementServlet.java
@@ -21,7 +21,7 @@ import unmsm.edu.pe.calidadsw.dao.model.Event;
  *
  * @author antony
  */
-@WebServlet(name = "events", urlPatterns = { "/events" })
+@WebServlet(name = "events", urlPatterns = {"/events"})
 public class EventManagementServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -32,10 +32,10 @@ public class EventManagementServlet extends HttpServlet {
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request  servlet request
+     * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
+     * @throws IOException if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -46,17 +46,17 @@ public class EventManagementServlet extends HttpServlet {
 
         try {
             switch (action) {
-            case "index":
-                index(request, response);
-                break;
-            case "delete":
-                delete(request, response);
-                break;
-            case "publish":
-                publish(request, response);
-                break;
-            default:
-                break;
+                case "index":
+                    index(request, response);
+                    break;
+                case "delete":
+                    delete(request, response);
+                    break;
+                case "publish":
+                    publish(request, response);
+                    break;
+                default:
+                    break;
             }
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, e.getMessage());
@@ -67,10 +67,10 @@ public class EventManagementServlet extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request  servlet request
+     * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
+     * @throws IOException if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -87,23 +87,37 @@ public class EventManagementServlet extends HttpServlet {
             Date date1 = format.parse(startDate);
             Date date2 = format.parse(endDate);
 
-            if (date1.compareTo(date2) != 1) {
-                if (date1.compareTo(date2) != 0) {
+            if (date1.compareTo(date2) == 0) {
+                request.setAttribute("errorMsg", "<div class='alert alert-danger' role='alert'>"
+                        + "Las fechas no pueden ser iguales.</div>");
+                request.getRequestDispatcher("eventManagement.jsp").forward(request, response);
+            } else {
+                if (date1.compareTo(date2) < 0) {
                     elements = eventDAO.search(startDate, endDate);
                     request.setAttribute("eventos", elements);
                     request.getRequestDispatcher("eventManagement.jsp").forward(request, response);
                 } else {
-                    request.setAttribute("errorMsg", "<div class='alert alert-danger' role='alert'>"
-                            + "Las fechas no pueden ser iguales.</div>");
+                    request.setAttribute("errorMsg", "<div class='alert alert-danger' role='alert'>La fecha de inicio no puede ser posterior"
+                            + " a la final.</div>");
+                    request.getRequestDispatcher("eventManagement.jsp").forward(request, response);
+                }
+            }
+            /*
+            if (date1.compareTo(date2) < 0) {
+                if (date1.compareTo(date2) == 0) {
+
+                } else {
+                    elements = eventDAO.search(startDate, endDate);
+                    request.setAttribute("eventos", elements);
                     request.getRequestDispatcher("eventManagement.jsp").forward(request, response);
                 }
             } else {
 
                 request.setAttribute("errorMsg", "<div class='alert alert-danger' role='alert'>La fecha de inicio no puede ser posterior"
-                        + " a la final.</div>");
+                        + " a la final.</div>" + startDate + " " + endDate);
                 request.getRequestDispatcher("eventManagement.jsp").forward(request, response);
             }
-
+            */
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, e.getMessage());
         }
